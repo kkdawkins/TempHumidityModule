4e7df231b783d165690fb4c1dff4f989bb1acdd4
Kevin Dawkins <kkdawkins@gmail.com>
2019-07-22 14:26:06 -0700

    Add project files.

---
+++ TempHumidModule/DataMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace TempHumidModule
+{
+    public class DataMessage
+    {
+        // T=39.0H=12.0\nT:39.0H:12.0\nT:39.0H:12.0\nT:39.0H:12.0
+        public DataMessage(string raw)
+        {
+
+        }
+
+        public string Temperature { get; }
+
+        public string Humidity { get; }
+    }
+}
